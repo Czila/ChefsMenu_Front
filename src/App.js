e1199b4de5e40a7d2393808e90c01a4d3c423616
Czila <lcesar.mk@gmail.com>
2022-07-26 10:20:24 +0200

    css inscription

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import AjouterunElement from './components/AjouterunElement/AjouterunElement';
 //import GenererunQRCode from './pages/GenererunQRCode/GenererunQRCode';
 import AccueilLogin from './pages/AccueilLogin/AccueilLogin';
 import AjouteruneCategorie from './components/AjouteruneCategorie/AjouteruneCategorie';
-
+import Inscription from './components/Inscription/Inscription'
 import AjouterunMenu from './components/AjouterunMenu/AjouterunMenu';
 import MaCarte from './pages/MaCarte/MaCarte';
 //import AjoutRestaurant from './components/AjoutRestaurant/AjoutRestaurant';
@@ -36,7 +36,7 @@ function App() {
         <button onClick={()=> logOut() }>Logout</button>
       </div>
       }
- <AjouterunElement />
+ <Inscription />
 
     </div>
   );
