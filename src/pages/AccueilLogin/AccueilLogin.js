d7f90f9885f1e7e4d5437f655ca5a962096087ab
Czila <lcesar.mk@gmail.com>
2022-07-28 16:48:30 +0200

    css inscription, login, accueillogin

--- src/pages/AccueilLogin/AccueilLogin.js
+++ src/pages/AccueilLogin/AccueilLogin.js
@@ -1,29 +1,17 @@
 import './AccueilLogin.css'
-import { useEffect, useState } from 'react';
-import {fetchWrapper} from '../../lib/useGestDB';
 import welcome from '../../assets/welcome.gif'
 
 
 function AccueilLogin(){  
-    const idRestaurateur=localStorage.getItem("userId");
-    const [restaurateur,setRestaurateur]=useState({});
-    
-    const getNomRestaurateur = async () => {
-        const NP = (await fetchWrapper.get(`http://localhost:3001/restaurateur/${idRestaurateur}`))
-        setRestaurateur(NP)
-    }
-useEffect( () => {
-    getNomRestaurateur()
-})
-    
+
     return(
         <div id="AccueilLogin">
             <div>
           <img src={welcome} alt="welcome" className='logo' /><br/><br/>
         </div>
             <div className='ALchoices'>
-                <label className='ALbienvenue'><h3>Bienvenue {`${restaurateur[0].prenom}`} {`${restaurateur[0].nom}`}<br/>
-                Sélectionnez votre restaurant </h3></label>
+                <label className='ALbienvenue'><h3>Bienvenue sur votre espace personnel !<br/>
+                Sélectionnez votre restaurant dans la liste ci-dessus</h3></label>
             </div>
         </div>
     
