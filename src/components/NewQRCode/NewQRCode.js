1e78a9c318acb98939d71bf56a5882b5865c2e7a
Rabat martin <martin.rabat@gmail.com>
2022-07-29 12:46:39 +0200

    debug qrcode

--- src/components/NewQRCode/NewQRCode.js
+++ src/components/NewQRCode/NewQRCode.js
@@ -12,25 +12,29 @@ function NewQRCode() {
   let idRestaurant =params.restaurantID
   const urlBase = `http://localhost:3000/restaurant/${idRestaurant}`
   const [table,setTables]=useState([{}])
+  let nbTable=0
 
   async function updateTables() {
     const t=[]
     const R = await fetchWrapper.get(`http://localhost:3001/restaurant/${idRestaurant}`)
-    for (let i =1 ; i< R[0].nbTable +1; i++)      
+    nbTable=R[0].nbTable
+    for (let i =1 ; i< nbTable +1; i++)      
     {   
         t.push({numTable:i, QRText : urlBase+i})
     }
     setTables(t)       
 }
 
-const printQRCode =() => {
-  const input = document.getElementById('QRCodelist');
+const printQRCode = async () => {
+  const input = document.getElementById('QRCodelistPrt');
+  input.removeAttribute("hidden");
   html2canvas(input)
     .then((canvas) => {
       const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF();
+      const pdf = new jsPDF();     
       pdf.addImage(imgData, 'JPEG', 0, 0);
-      // pdf.output('dataurlnewwindow');
+      pdf.autoPrint();
+      pdf.output('dataurlnewwindow');
       pdf.save("qRcode.pdf");
     })
 }
@@ -50,7 +54,15 @@ useEffect(() => {
           <QRCode size='250' className="QRCode" key={t.numTable} value={t.QRText} renderAs="svg" /> 
         </div>
       ))}
-    </div>  
+    </div> 
+    <div id="QRCodelistPrt" className="QRCodelistPrt" >
+      {(table.map((t) =>  
+        <div  >
+          <h4 id="QRCodetitre" >Table {t.numTable}</h4>
+          <QRCode size='150' key={t.numTable} value={t.QRText} renderAs="svg" /> 
+        </div>
+      ))}
+    </div> 
     </div>
 
     </div>
